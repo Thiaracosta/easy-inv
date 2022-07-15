aceb01b96913a5ea9be2aea02b5dcd4eb0e237cd
thiara <thiaramscosta@gmail.com>
2022-07-15 16:49:32 -0300

    feat: criação das rotas - app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá, easyInv!</h1>
-    </div>
+    <Switch>
+      <Route exact path="/listActions" component={ ListActions } />
+      <Route exact path="/buyAndSell" component={ BuyAndSell } />
+      <Route exact path="/account" component={ Account } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/" component={ Login } />
+      <Route path="/*" component={ NotFound } />
+    </Switch>
   );
 }
 
