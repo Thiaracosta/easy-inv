9f8ed7be01c182b2979434be6ce1efb09b7f3e06
thiara <thiaramscosta@gmail.com>
2022-07-16 22:40:07 -0300

    style: estilização da tabela - table.css

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import invContext from '../context/invContext';
 import {  useNavigate } from 'react-router-dom';
+import './table.css'
 
 function Table(props) {
   const navigate =  useNavigate();
@@ -14,45 +15,49 @@ function Table(props) {
   }
 
   return (
-    <>
-      <p>{title}</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Ação</th>
-            <th>Quant</th>
-            <th>Valor</th>
-            <th>Negociar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {actions.map((item) => (
-            <tr key={ item.company }>
-              <td>{ item.company }</td>
-              <td>{ item.quantity }</td>
-              <td>{ item.price }</td>
-              <td>
-                <button
-                  type="button"
-                  value={ item.company }
-                  onClick={ handleBuyAndSell }
-                >
-                  C
-                </button>
-                <button
-                  type="button"
-                  value={ item.company }
-                  onClick={ handleBuyAndSell }
-                  disabled={isVisible}
-                >
-                  V
-                </button>
-              </td>
+    <main>
+    <p className='title-table'>{title}</p>
+     <div className='container-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Ação</th>
+              <th>Quant</th>
+              <th>Valor</th>
+              <th>Negociar</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+          </thead>
+          <tbody>
+            {actions.map((item) => (
+              <tr key={ item.company }>
+                <td>{ item.company }</td>
+                <td>{ item.quantity }</td>
+                <td>{ item.price }</td>
+                <td className='tr-card-btn'>
+                  <button
+                    className='bnt-table btn-green'
+                    type="button"
+                    value={ item.company }
+                    onClick={ handleBuyAndSell }
+                  >
+                    C
+                  </button>
+                  <button
+                    className='bnt-table btn-blue'
+                    type="button"
+                    value={ item.company }
+                    onClick={ handleBuyAndSell }
+                    disabled={isVisible}
+                  >
+                    V
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
     )
 
 
